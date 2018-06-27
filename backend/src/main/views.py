7fa9f6a37ff843debd2020762fdffd3e2ccb0d34
Jorge Fuentes <jsjfuentesj@yahoo.com>
2018-06-27 02:03:37 -0700

    Add Template and Forms, not working

--- backend/src/main/views.py
+++ backend/src/main/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, redirect, url_for, jsonify, request
-from flask_login import login_required, current_user
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user, login_user
 
 from ..forms import RegistrationForm
 
@@ -12,8 +12,17 @@ def index():
 @main.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(form.username.data, form.email.data,
-                    form.password.data)
+    print("Hit", request.method)
+    flash("HI")
+
+    if request.method == 'POST':
+        if not form.validate():
+            flash("Form validation fails")
+            return render_template('register.html', form=form)
+        user = User(form.email.data, form.password.data)
+        print("HERE", user.email)
+        user.save()
+        login_user(user)
+        flash('Thanks for registering')
         return redirect(url_for('index'))
     return render_template('register.html', form=form)

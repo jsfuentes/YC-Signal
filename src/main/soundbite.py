4db9470560f51f87af4cecf03bef17e96f1633c7
Jorge Fuentes <jsjfuentesj@gmail.com>
2019-04-13 12:12:25 -0700

    Json

--- src/main/soundbite.py
+++ src/main/soundbite.py
@@ -9,7 +9,10 @@ from ..models import Soundbites, Episodes
 soundbite = Blueprint('soundbite', __name__)
 s3 = boto3.client('s3')
 
-
+@soundbite.route('/search', methods = ['GET', 'POST'])
+def search():
+    print(request.get_json())
+    return "hello"
 
 @soundbite.route('/data', methods = ['GET', 'POST'])
 def data():
@@ -32,23 +35,24 @@ def data():
     # f = request.files['audio']
     # key = secrets.token_hex(nbytes=16)
     # s3.upload_fileobj(f, 'yc-signal-soundbite-audio', key)
-    form = request.form
-    episode = Episodes.objects(id=form['episode']).first()
+    r_data = request.get_json()
+    print("R_DATA", r_data)
+    episode = Episodes.objects(id=r_data['episode']).first()
     
     #Create Clip
     s3.download_file("yc-signal-episode-audio", episode.s3_audio_key, "temp.mp3")
     episode_audio = AudioSegment.from_mp3("temp.mp3")
 
-    s = int(form['start_time'])
-    e = int(form['end_time'])
+    s = int(r_data['start_time'])
+    e = int(r_data['end_time'])
     soundbite_audio = episode_audio[s:e]
     soundbite_audio.export("./temp2.mp3", format="mp3")
     length = len(soundbite_audio)
     
     key = secrets.token_hex(nbytes=16)
     s3.upload_file('temp2.mp3', 'yc-signal-soundbite-audio', key)
-    soundbite = Soundbites(title=form['title'], 
-                        episode=form['episode'],
+    soundbite = Soundbites(title=r_data['title'], 
+                        episode=r_data['episode'],
                         podcast=episode.podcast,
                         length=length,
                         s3_audio_key=key)
